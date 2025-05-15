77ee8bcc3945faaf3510aec8c30e7c1ff7fc3ff6
Lujiang <lujiang0111@gmail.com>
2025-05-15 11:10:09 +0800

    添加更多判断

--- make_env.bat
+++ make_env.bat
@@ -1,9 +1,14 @@
 @echo off
 
 :: 文件框架
-mkdir source\lib
-mkdir source\program
-mkdir version\lib
+mkdir "source\lib" >nul 2>&1
+mkdir "source\program" >nul 2>&1
+mkdir "version\lib" >nul 2>&1
 
 :: lccl
-git clone https://github.com/Lujiang0111/lccl.git source/lib/lccl
+if not exist "source\lib\lccl" (
+    git clone https://github.com/Lujiang0111/lccl.git source\lib\lccl
+)
+
+echo done.
+pause
