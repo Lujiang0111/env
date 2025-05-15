c9fd58dda9d2b7b6e60b0135e77f22cdae37aa10
Lujiang <lujiang0111@gmail.com>
2025-05-15 11:15:04 +0800

    添加更多注释

--- make_env.bat
+++ make_env.bat
@@ -5,16 +5,17 @@ mkdir "source\lib" >nul 2>&1
 mkdir "source\program" >nul 2>&1
 mkdir "version\lib" >nul 2>&1
 
-:: lccl
+:: lib
+::: lccl
 if not exist "source\lib\lccl" (
     git clone https://github.com/Lujiang0111/lccl.git source\lib\lccl
 )
 
-:: pcap_recorder2
+::program
+::: pcap_recorder2
 if not exist "source\program\pcap_recorder2" (
     git clone https://github.com/Lujiang0111/pcap_recorder2.git source\program\pcap_recorder2
 )
-
 
 echo done.
 pause
