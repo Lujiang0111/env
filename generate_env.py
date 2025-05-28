dc3edc7178ea9bb773c885dfb99412ccebe15bb8
Lujiang <lujiang0111@gmail.com>
2025-05-28 09:45:20 +0800

    更新健壮性

--- generate_env.py
+++ generate_env.py
@@ -1,16 +1,20 @@
+import os
+import pathlib
 import subprocess
-from pathlib import Path
 
 
 def git_clone_if_missing(repo_url, target_dir):
-    if not Path(target_dir).exists():
+    if not pathlib.Path(target_dir).exists():
         subprocess.run(["git", "clone", repo_url, target_dir], check=True)
 
 
+env_path = pathlib.Path(__file__).resolve().parent
+os.chdir(env_path)
+
 # 创建目录结构
-Path("source/lib").mkdir(parents=True, exist_ok=True)
-Path("source/program").mkdir(parents=True, exist_ok=True)
-Path("version/lib").mkdir(parents=True, exist_ok=True)
+pathlib.Path("source/lib").mkdir(parents=True, exist_ok=True)
+pathlib.Path("source/program").mkdir(parents=True, exist_ok=True)
+pathlib.Path("version/lib").mkdir(parents=True, exist_ok=True)
 
 # source/lib
 git_clone_if_missing("https://github.com/Lujiang0111/lccl.git", "source/lib/lccl")
@@ -26,4 +30,4 @@ git_clone_if_missing(
     "https://github.com/Lujiang0111/pcap_recorder2.git", "source/program/pcap_recorder2"
 )
 
-print("done.")
+print("generate done.")
