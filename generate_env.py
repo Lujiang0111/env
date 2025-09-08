2df69d6d94e075764b86fe6575048e0d5dd59aa0
lujiang <lujiang0111@gmail.com>
2025-09-08 11:09:12 +0800

    若目录存在，则更新

--- generate_env.py
+++ generate_env.py
@@ -3,34 +3,37 @@ import pathlib
 import subprocess
 
 
-def git_clone_if_missing(repo_url, target_dir):
-    if not pathlib.Path(target_dir).exists():
+def git_sync(repo_url, target_dir):
+    if pathlib.Path(target_dir).exists():
+        subprocess.run(["git", "-C", target_dir, "pull", "origin", "main"], check=True)
+    else:
         subprocess.run(["git", "clone", repo_url, target_dir], check=True)
 
 
-env_path = pathlib.Path(__file__).resolve().parent
-os.chdir(env_path)
+if __name__ == "__main__":
+    env_path = pathlib.Path(__file__).resolve().parent
+    os.chdir(env_path)
 
-# 创建目录结构
-pathlib.Path("source/lib").mkdir(parents=True, exist_ok=True)
-pathlib.Path("source/program").mkdir(parents=True, exist_ok=True)
-pathlib.Path("version/lib").mkdir(parents=True, exist_ok=True)
+    # 更新env自身
+    git_sync("https://github.com/Lujiang0111/env.git", ".")
 
-# source/lib
-git_clone_if_missing("https://github.com/Lujiang0111/lccl.git", "source/lib/lccl")
-git_clone_if_missing(
-    "https://github.com/Lujiang0111/pcap_dump.git", "source/lib/pcap_dump"
-)
-git_clone_if_missing(
-    "https://github.com/Lujiang0111/ts_transmit.git", "source/lib/ts_transmit"
-)
+    # 创建目录结构
+    pathlib.Path("source/lib").mkdir(parents=True, exist_ok=True)
+    pathlib.Path("source/program").mkdir(parents=True, exist_ok=True)
+    pathlib.Path("version/lib").mkdir(parents=True, exist_ok=True)
 
-# source/program
-git_clone_if_missing(
-    "https://github.com/Lujiang0111/pcap_recorder2.git", "source/program/pcap_recorder2"
-)
-git_clone_if_missing(
-    "https://github.com/Lujiang0111/udp_recorder.git", "source/program/udp_recorder"
-)
+    # source/lib
+    git_sync("https://github.com/Lujiang0111/lccl.git", "source/lib/lccl")
+    git_sync("https://github.com/Lujiang0111/pcap_dump.git", "source/lib/pcap_dump")
+    git_sync("https://github.com/Lujiang0111/ts_transmit.git", "source/lib/ts_transmit")
 
-print("generate done.")
+    # source/program
+    git_sync(
+        "https://github.com/Lujiang0111/pcap_recorder2.git",
+        "source/program/pcap_recorder2",
+    )
+    git_sync(
+        "https://github.com/Lujiang0111/udp_recorder.git", "source/program/udp_recorder"
+    )
+
+    print("generate done.")
